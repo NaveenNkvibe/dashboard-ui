a5c5ddc0d2527ba9f19afcd88ca84f3b5130754f
Naveen <naveen@confyans.com>
2026-02-12 16:34:31 +0530

    add sidebar close function when clicked anywhere outside

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,32 @@
 import { Outlet } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Sidebar from '../components/sidebar/Sidebar';
 import Navbar from '../components/navbar/Navbar';
+import { useRef } from 'react';
 
 const MainLayout = () => {
 	const [isOpen, setIsOpen] = useState(true); // Boolean values to toggle sidebar open/close state
+	const sidebarRef = useRef(null);
+
+	useEffect(() => {
+		function handleClickOutside(event) {
+			if (isOpen && sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+				setIsOpen(false);
+			}
+		} // Checks clicks outside of sidebar
+
+		document.addEventListener('mousedown', handleClickOutside); // Event listener to check clicks anywhere on the page
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside); // clean up function
+		};
+	}, [isOpen]);
 
 	return (
 		<div className="flex">
-			<Sidebar isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} />
+			<div ref={sidebarRef}>
+				<Sidebar isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} />
+			</div>
 			<div className={`bg-[#E7E7E7] min-h-screen flex-1 overflow-x-hidden transition-all duration-300 p-8 ${isOpen ? 'md:ml-64' : 'ml-16'}`}>
 				{/* <div className="flex-1 bg-[#E7E7E7] min-h-screen"> */}
 				<Navbar />
